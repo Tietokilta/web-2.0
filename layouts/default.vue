2c662e4ba83caa01b177b1aebbab0f83a0dcc386
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-07-21 12:28:49 +0300

    lint

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,18 @@
   <div class="container">
     <nav class="navbar">
       <div class="navbar_menu">
-        <nuxt-link class="navbar__item" to="/guild">{{ $t('navigation.guild') }}</nuxt-link>
-        <nuxt-link class="navbar__item" to="/freshmen">{{ $t('navigation.freshmen') }}</nuxt-link>
-        <nuxt-link class="navbar__item" to="/abi">{{ $t('navigation.abi') }}</nuxt-link>
-        <nuxt-link class="navbar__item" to="/companies">{{ $t('navigation.companies') }}</nuxt-link>
+        <nuxt-link class="navbar__item" to="/guild">
+          {{ $t('navigation.guild') }}
+        </nuxt-link>
+        <nuxt-link class="navbar__item" to="/freshmen">
+          {{ $t('navigation.freshmen') }}
+        </nuxt-link>
+        <nuxt-link class="navbar__item" to="/abi">
+          {{ $t('navigation.abi') }}
+        </nuxt-link>
+        <nuxt-link class="navbar__item" to="/companies">
+          {{ $t('navigation.companies') }}
+        </nuxt-link>
       </div>
     </nav>
     <nuxt />
