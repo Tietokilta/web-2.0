eed24fe6ccffa0b16ee2f1f4320ad2a5267344c9
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-09-08 04:17:12 +0300

    Justify navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
     <nav class="navbar">
-      <div class="navbar_menu">
+      <div class="navbar__icon">
         <nuxt-link class="navbar__item" to="/guild">
           {{ $t('navigation.guild') }}
         </nuxt-link>
+      </div>
+      <div class="navbar__menu">
         <nuxt-link class="navbar__item" to="/freshmen">
           {{ $t('navigation.freshmen') }}
         </nuxt-link>
@@ -13,6 +15,17 @@
         </nuxt-link>
         <nuxt-link class="navbar__item" to="/companies">
           {{ $t('navigation.companies') }}
+        </nuxt-link>
+      </div>
+      <div class="navbar__lang">
+        <nuxt-link class="navbar__item" to="/fi">
+          {{ $t('navigation.fi') }}
+        </nuxt-link>
+        <span class="navbar__item">
+        /
+        </span>
+        <nuxt-link class="navbar__item" to="/en">
+          {{ $t('navigation.en') }}
         </nuxt-link>
       </div>
     </nav>
