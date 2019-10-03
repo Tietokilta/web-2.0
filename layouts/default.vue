405b295b62b095ec28702faf0ae6babfbb531732
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-10-04 00:22:10 +0300

    make navbar nice

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,32 +2,32 @@
   <div class="container">
     <nav class="navbar">
       <div class="navbar__icon">
-        <nuxt-link class="navbar__item" to="/guild">
+        <nuxt-link class="navbar__item navbar__clickable" to="/guild">
           {{ $t('navigation.guild') }}
         </nuxt-link>
       </div>
       <div class="navbar__menu">
-        <nuxt-link class="navbar__item" to="/freshmen">
+        <nuxt-link class="navbar__item navbar__clickable" to="/freshmen">
           {{ $t('navigation.freshmen') }}
         </nuxt-link>
-        <nuxt-link class="navbar__item" to="/abi">
+        <nuxt-link class="navbar__item navbar__clickable" to="/abi">
           {{ $t('navigation.abi') }}
         </nuxt-link>
-        <nuxt-link class="navbar__item" to="/members">
+        <nuxt-link class="navbar__item navbar__clickable" to="/members">
           {{ $t('navigation.members') }}
         </nuxt-link>
-        <nuxt-link class="navbar__item" to="/companies">
+        <nuxt-link class="navbar__item navbar__clickable" to="/companies">
           {{ $t('navigation.companies') }}
         </nuxt-link>
       </div>
       <div class="navbar__lang">
-        <nuxt-link class="navbar__item" to="/">
+        <nuxt-link class="navbar__item navbar__clickable" to="/">
           {{ $t('navigation.fi') }}
         </nuxt-link>
         <span class="navbar__item">
           /
         </span>
-        <nuxt-link class="navbar__item" to="/en">
+        <nuxt-link class="navbar__item navbar__clickable" to="/en">
           {{ $t('navigation.en') }}
         </nuxt-link>
       </div>
