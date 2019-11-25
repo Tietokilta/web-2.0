157175b7538c61ff0c264a8cac598ee7c4263aaa
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-11-25 16:08:43 +0200

    add burger

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,24 +6,6 @@
           {{ $t('navigation.guild') }}
         </nuxt-link>
       </div>
-      <label class="burger">
-        <input type="checkbox" />
-        <span>
-          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('freshmen')">
-            {{ $t('navigation.freshmen') }}
-          </nuxt-link>
-          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('abi')">
-            {{ $t('navigation.abi') }}
-          </nuxt-link>
-          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('members')">
-            {{ $t('navigation.members') }}
-          </nuxt-link>
-          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('companies')">
-            {{ $t('navigation.companies') }}
-          </nuxt-link>
-          <v-locale class="navbar__item navbar__clickable" />
-        </span>
-      </label>
       <div class="navbar__menu">
         <nuxt-link class="navbar__item navbar__clickable" :to="localePath('freshmen')">
           {{ $t('navigation.freshmen') }}
@@ -40,6 +22,25 @@
       </div>
       <div class="navbar__lang">
         <v-locale class="navbar__item navbar__clickable" />
+      </div>
+
+      <div class="burger__menu">
+        <input type="checkbox" checked="true" id="burger_button" />
+        <label for="burger_button" class="burger"/>
+        <span>
+          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('freshmen')">
+            {{ $t('navigation.freshmen') }}
+          </nuxt-link>
+          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('abi')">
+            {{ $t('navigation.abi') }}
+          </nuxt-link>
+          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('members')">
+            {{ $t('navigation.members') }}
+          </nuxt-link>
+          <nuxt-link class="navbar__item navbar__clickable" :to="localePath('companies')">
+            {{ $t('navigation.companies') }}
+          </nuxt-link>
+        </span>
       </div>
     </nav>
     <nuxt />
